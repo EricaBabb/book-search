8333b44a7d0d325cfaef11558559db4a42d182d7
EricaBabb <ecamilebabb@gmail.com>
2021-09-03 19:31:57 -0400

    schemas and npm installs

---
+++ server/schemas/typeDefs.js
@@ -0,0 +1,43 @@
+const { gql } = require('apollo-server-express');
+
+const typeDefs = gql`
+  type User {
+    _id: ID
+    username: String
+    email: String
+    books: [Book]
+
+  }
+
+  type Book {
+    _id: ID
+    authors: String
+    description: String
+    image: String
+    link: String
+    title: String
+ 
+  }
+
+
+  type Auth {
+    token: ID!
+    user: User
+  }
+
+  type Query {
+    me: User
+    users: [User]
+    user(username: String!): User
+    books(username: String): [Book]
+    book(_id: ID!): Book
+  }
+
+  type Mutation {
+    login(email: String!, password: String!): Auth
+    addUser(username: String!, email: String!, password: String!): Auth
+    addBook(bookText: String!): Book
+  }
+`;
+
+module.exports = typeDefs;
